60c6779674375ca7405512eb35ab07f4e996fd8d
Sulymaani <saleebanmaxamed7@gmail.com>
2023-03-20 23:30:39 +0300

    woah

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - A c file to display a string to the standard err.
+ * Return - 0 signifying program success
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
+
